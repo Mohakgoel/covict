8f5ce606f09550cbbce26a2ad326252a4ad7fa1b
Mohakgoel <mohakgoel9@gmail.com>
2021-03-05 00:20:02 +0530

    STATIC

--- covict/settings.py
+++ covict/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-Static_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'_staticfiles_')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
